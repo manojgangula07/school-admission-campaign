1e2ac9fba014c452a0be9c16246047e564e05c63
manojgangula07 <manojgangula02@gmail.com>
2025-04-30 22:15:47 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config, ProductionConfig
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -9,5 +10,9 @@ def create_app():
     else:
         app.config.from_object(Config)
 
-    # Initialize extensions like db here
+    # Initialize extensions like db here (e.g. db.init_app(app))
+
     return app
+
+# Create the app instance and assign it to 'app'
+app = create_app()
