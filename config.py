28e0b16a3c5e1b7e70f516d59c83877ad980d9be
manojgangula07 <manojgangula02@gmail.com>
2025-04-30 22:38:22 +0530

    app updated

--- config.py
+++ config.py
@@ -10,16 +10,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    DEBUG = True
-
-class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL')
     DEBUG = False
 
-class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL') or 'sqlite:///:memory:'
-    TESTING = True
-    DEBUG = True
+# class DevelopmentConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+#     DEBUG = True
+
+# class ProductionConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL')
+#     DEBUG = False
+
+# class TestingConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL') or 'sqlite:///:memory:'
+#     TESTING = True
+#     DEBUG = True
--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import os
-from flask import Flask, render_template
-from config import Config, ProductionConfig
-
-def create_app():
-    app = Flask(__name__)
-
-    if os.getenv("FLASK_ENV") == "production":
-        app.config.from_object(ProductionConfig)
-    else:
-        app.config.from_object(Config)
-
-    # Initialize routes
-    with app.app_context():
-        from app import routes  # Ensure routes are correctly imported
-
-    return app
+from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
